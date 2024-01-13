4f800cddee948b9f975588ec071760c71d6bcf6a
huanxi <2290991129@qq.com>
2024-01-13 18:13:18 +0800

    使用vitest做测试

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from './core/React.js'
 
 const App = React.createElement('div', { id: 'app' }, 'hi', ' ', React.createTextNode('react'))
+console.log(React.createTextNode('react'))
 
 export default App
