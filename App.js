b81339aaecaa7f328b60bdb43a1f717412b1965a
huanxi <2290991129@qq.com>
2024-01-13 16:31:40 +0800

    实现最简mini-react

---
+++ App.js
@@ -0,0 +1,5 @@
+import React from './core/React.js'
+
+const App = React.createElement('div', { id: 'app' }, 'hi', ' ', React.createTextNode('react'))
+
+export default App
---
+++ main.js
@@ -0,0 +1,4 @@
+import App from './App.js'
+import ReactDOM from './core/ReactDom.js'
+
+ReactDOM.createRoot(document.getElementById('root')).render(App)
